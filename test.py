596586f135dedd2c8a05dac6d9101ee8ab3e0dc1
chemelnucfin <alan.c.wu@gmail.com>
2018-05-28 22:21:15 -0700

    Commit 1 - Add comments, convert to python3 and make fizz_buzz more general

--- test.py
+++ test.py
@@ -1,26 +1,51 @@
 import csv
 import json
+import subprocess
+import tempfile
+import unittest
 
-
-class TestClass(object):
+class TestClass:
 
     def __init__(self, foo, bar, baz):
+        """Initializes foo bar and baz
+
+        Params:
+            foo: foo
+            bar: bar
+            baz: baz
+        """
         self.foo = foo
         self.bar = bar
         self.baz = baz
 
     @staticmethod
-    def fizz_buzz(digit_1, digit_2):
-        for i in range(1, 100):
+    def fizz_buzz(digit_1, digit_2, begin=1, end=101):
+        """Prints number or some combination of fizz and buzz
+
+        Params:
+            digit_1: the number evenly divided that gives fizz
+            digit_2: the number evenly divided that gives buzz
+        """
+        output = []
+        for i in range(begin, end):
             if i % digit_1 == 0:
                 if i % digit_2 == 0:
-                    print 'fizzbuzz!'
+                    output.append('fizzbuzz!')
                 else:
-                    print 'fizz!'
+                    output.append('fizz!')
             elif i % digit_2 == 0:
-                print 'buzz!'
+                output.append('buzz!')
             else:
-                print i
+                output.append(str(i))
+        return output
+
+    def print_list(self, item_list):
+        """Prints a list of items separated by '\n'
+
+        Params:
+            item_list: the list of items to print
+        """
+        print('\n'.join(item_list))
 
     def oh_this_is_trouble(self, param1, param2):
         """Oh man this fucntion is trouble"""
@@ -34,13 +59,17 @@ class TestClass(object):
         """Convert a file containing a list of flat JSON objects to a csv.
 
         What's a DictWriter, you say? Never heard of it!
-
+        Params:
+            json_file_path: the path to json
+            outfile_path: the output csv file_path
         """
         with open(json_file_path) as f:
-            data = json.load(f)
+            data = f.read()
+        data = json.loads(data)
+        
         with open(outfile_path, 'w') as fp:
-            writer = csv.writer(fp)
-            writer.writerow(data[0].keys())
+            writer = csv.DictWriter(fp, fieldnames=data[0].keys())
+            writer.writeheader()
             for item in data:
                 writer.writerow(item.values())
 
