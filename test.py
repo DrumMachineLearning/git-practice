1483f30d468ad28dda430ac1704e7c3ade2ae89d
chemelnucfin <alan.c.wu@gmail.com>
2018-05-28 22:01:19 -0700

    Commit 2 - Add tests

--- test.py
+++ test.py
@@ -50,9 +50,17 @@ class TestClass:
     def oh_this_is_trouble(self, param1, param2):
         """Oh man this fucntion is trouble"""
         for i in range(20):
-            print 'trouble' + i * '!'
+            print('trouble' + i * '!')
             for j in range(10):
-                print i * j
+                print(i * j)
+
+    def test_fizz_buzz(self):
+        """ Runs fizz_buzz test"""
+        answer = [
+            '1', '2', 'fizz!', '4', 'buzz!', 'fizz!', '7', '8',
+            'fizz!', 'buzz!', '11', 'fizz!', '13', '14', 'fizzbuzz!'
+        ]
+        assert self.fizz_buzz(3, 5, 1, 16) == answer
 
     @staticmethod
     def json_to_csv(json_file_path, outfile_path):
@@ -71,8 +79,34 @@ class TestClass:
             writer = csv.DictWriter(fp, fieldnames=data[0].keys())
             writer.writeheader()
             for item in data:
-                writer.writerow(item.values())
+                writer.writerow(item)
+
+    def test_json_to_csv(self):
+        """Runs json_to_csv test"""
+        json_file_path = tempfile.NamedTemporaryFile("w")
+        outfile_path = tempfile.NamedTemporaryFile("w")
+        test_json = [{'a': '1', 'b': '2'}, {'a': '3', 'b': '4'}]
+        test_json = json.dumps(test_json)
+
+        json_file_path.write(test_json)
+        json_file_path.flush()
+        with open(json_file_path.name) as f:
+            data = f.read()
+
+        self.json_to_csv(json_file_path.name, outfile_path.name)
+        output = []
+        with open(outfile_path.name) as outfile:
+            reader = csv.DictReader(outfile)
+            for row in reader:
+                output.append(row)
+
+        # output is a list of ordered_dicts now
+        output = [dict(ordered_dict) for ordered_dict in output]
+        output = json.dumps(output)
+        assert test_json == output
 
 if __name__ == '__main__':
     t = TestClass(1, 2, 3)
     TestClass.fizz_buzz(3, 5)
+    t.test_fizz_buzz()
+    t.test_json_to_csv()
