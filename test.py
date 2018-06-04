7b9ca2e8186a9291ce50d7aef5b1a56eeafa8314
Alec Wong <alecwong@alecwong.mtv.corp.google.com>
2018-06-04 16:56:21 -0700

    add blah

--- test.py
+++ test.py
@@ -21,6 +21,12 @@ class TestClass(object):
             else:
                 print i
 
+    def blah(self):
+        print('blah')
+        print('blah')
+        print('blah')
+        print('blah')
+
     def json_to_csv(self, json_file_path, outfile_path):
         """Convert a file containing a list of flat JSON objects to a csv.
 
