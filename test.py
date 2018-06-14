9990e72a0b201c923eebc85d3bdf2739fbf3bbbf
Suchendra <suchendrakuchipudi@gmail.com>
2018-06-13 22:15:09 -0700

    method 2 added

--- test.py
+++ test.py
@@ -29,6 +29,14 @@ class TestClass(object):
 
         print j
 
+    def method_2():
+        j=[]
+        k=10
+        for i in range(1, 100):
+            j.append(i-k)
+
+        print j
+
     def json_to_csv(self, json_file_path, outfile_path):
         """Convert a file containing a list of flat JSON objects to a csv.
 
@@ -45,3 +53,4 @@ class TestClass(object):
 
 
     method_1()
+    method_2()
