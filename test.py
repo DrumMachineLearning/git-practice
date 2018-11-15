2846699dcdc476f11e73f29b22921db050bc4d55
Your Name <you@example.com>
2018-11-15 15:21:37 -0500

    Commited to Branch 1

--- test.py
+++ test.py
@@ -13,19 +13,25 @@ class TestClass(object):
         for i in range(1, 100):
             if i % digit_1 == 0:
                 if i % digit_2 == 0:
-                    print 'fizzbuzz!'
+                    print ('fizzbuzz!')
                 else:
-                    print 'fizz!'
+                    print ('fizz!')
             elif i % digit_2 == 0:
-                print 'buzz!'
+                print ('buzz!')
             else:
-                print i
+                print (i)
+
+    def bubble_sort(self, alist):
+        length = len(alist) - 1
+        for i in range(0, length):
+            if alist[i] > alist[i + 1]:
+                # alist[i], alist[i+1] = alist[i+1], alist[i]  # python way
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
 
     def json_to_csv(self, json_file_path, outfile_path):
-        """Convert a file containing a list of flat JSON objects to a csv.
-
-        What's a DictWriter, you say? Never heard of it!
-
+        """DictWriter has saved my life!!!
         """
         with open(json_file_path) as f:
             data = json.load(f)
@@ -34,3 +40,7 @@ class TestClass(object):
             writer.writerow(data[0].keys())
             for item in data:
                 writer.writerow(item.values())
+
+if __name__ == '__main__':
+    t = TestClass(1, 2, 3)
+    t.fizz_buzz(3, 5)
